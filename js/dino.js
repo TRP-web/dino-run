970d1555bd1b139935af6d3cb0349d6d0d6c800b
unknown <kusokpocki@gmail.com>
2022-04-06 22:58:52 +0300

    for mobile

--- js/dino.js
+++ js/dino.js
@@ -3,19 +3,28 @@ export class Dino {
         this.dinoElement = dinoElement
         document.addEventListener('keydown', (event) => {
             if (event.key === " " && !this.dinoElement.classList.contains('dino-jump')) {
-                this.dinoElement.classList.add('dino-jump')
-                setTimeout(() => {
-                    this.dinoElement.classList.remove("dino-jump")
-                }, 1300)
+                this.jump() 
+            }
+        })
+        document.addEventListener("click", () => {
+            if (!this.dinoElement.classList.contains('dino-jump')) {
+                this.jump()
             }
         })
         this.dinoElement.addEventListener("denger-zone", (event) => {
             let topDethPoint = event.detail.dethElement.getBoundingClientRect().top + window.pageXOffset + event.detail.dethElement.offsetHeight
             let topDino = this.dinoElement.getBoundingClientRect().top + window.pageXOffset + this.dinoElement.offsetHeight
-            if (Math.abs(topDino - topDethPoint)  > 22) {
+            if (Math.abs(topDino - topDethPoint) > 22) {
             } else {
                 window.location.reload()
             }
         })
+    }
+
+    jump() {
+        this.dinoElement.classList.add('dino-jump')
+        setTimeout(() => {
+            this.dinoElement.classList.remove("dino-jump")
+        }, 1300)
     }
 }
